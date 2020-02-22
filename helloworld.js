77e0e5f1f3e8d2a7c56520cee781697c22d1aac9
RedRabbitNet <redrabbitnet@gmail.com>
2020-02-22 21:45:17 +0900

    GrantUser
    ユーザにアイテムの付与を実装しました。

--- helloworld.js
+++ helloworld.js
@@ -18,5 +18,18 @@ handlers.getTableCall = function (args, context) {
     };
 
     var result = server.EvaluateRandomResultTable(request);
+    return result.ResultItemId;
+};
+
+// This is a simple example of making a PlayFab server API call
+handlers.itemToUser = function (args, context) {
+    var request = {
+        // Annotation: "GachaPresent",
+        // CatalogVersion: "Character",
+        ItemIds: args.itemIds,
+        PlayFabId: currentPlayerId,
+    };
+
+    var result = server.GrantItemsToUser(request);
     return result;
 };
