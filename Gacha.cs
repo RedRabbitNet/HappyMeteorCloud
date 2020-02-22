d509152e735158a0462efb3e572e6089aec53c45
RedRabbitNet <redrabbitnet@gmail.com>
2020-02-22 20:25:42 +0900

    staticCSandJS
    csファイルのクラスをstaticで定義してみました。
    jsで実行できるかのテストのためにファイルをアップロードしました。

--- Gacha.cs
+++ Gacha.cs
@@ -1,22 +1,25 @@
-public void Grant(string playFabId, string tableId) {
-    // First, roll a random number and evaluate the drop table
-    PlayFabServerAPI.EvaluateRandomResultTable(new EvaluateRandomResultTableRequest()
-    {
-        TableId = tableId
-    }, result => OnRandomResultTableResponse(result, playFabId), OnError);
-}
+public static class PlayFabServerAPI
+{
+    public static void Grant(string playFabId, string tableId) {
+        // First, roll a random number and evaluate the drop table
+        PlayFabServerAPI.EvaluateRandomResultTable(new EvaluateRandomResultTableRequest()
+        {
+            TableId = tableId
+        }, result => OnRandomResultTableResponse(result, playFabId), OnError);
+    }
 
-public void OnRandomResultTableResponse(EvaluateRandomResultTableResult tableResult, string playFabId) {
-    // Second, take the result and grant it to the player
-    PlayFabServerAPI.GrantItemsToUser(new GrantItemsToUserRequest {
-        PlayFabId = playFabId,
-        ItemIds = new List<string> { tableResult.ResultItemId }
-    }, result =>
-    {
-        // Handle Result
-    }, OnError);
-}
+    public static void OnRandomResultTableResponse(EvaluateRandomResultTableResult tableResult, string playFabId) {
+        // Second, take the result and grant it to the player
+        PlayFabServerAPI.GrantItemsToUser(new GrantItemsToUserRequest {
+            PlayFabId = playFabId,
+            ItemIds = new List<string> { tableResult.ResultItemId }
+        }, result =>
+        {
+            // Handle Result
+        }, OnError);
+    }
 
-public void OnError(PlayFabError error) {
-    Debug.LogError(error.GenerateErrorReport());
+    public static void OnError(PlayFabError error) {
+        Debug.LogError(error.GenerateErrorReport());
+    }
 }
